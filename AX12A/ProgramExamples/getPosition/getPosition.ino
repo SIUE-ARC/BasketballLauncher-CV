20e807a2c4b8835935d672d19d9c29d5a9b7d869
choneil <choneil@siue.edu>
2025-01-18 20:33:49 -0600

    Update and rename control.ino to getPosition.ino

---
+++ AX12A/ProgramExamples/getPosition/getPosition.ino
@@ -0,0 +1,19 @@
+#include <ax12.h>
+#include <BioloidController.h>
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop ()
+{
+  while (Serial.available() ==0){
+  }
+  int motorPosition = Serial.parseInt();
+  SetPosition(10,motorPosition);
+  delay(500);
+  int p = GetPosition(10);
+  Serial.print(p);
+  Serial.print("\n");
+}
