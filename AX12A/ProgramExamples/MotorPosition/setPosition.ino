2b67b677b145bd8b2f1edb5d5013f392de0b6daf
choneil <choneil@siue.edu>
2025-01-18 20:32:43 -0600

    Update and rename oh_my_god_it_worked.ino to setPosition.ino

---
+++ AX12A/ProgramExamples/MotorPosition/setPosition.ino
@@ -0,0 +1,25 @@
+#include <ax12.h>
+
+
+void setup ()
+{
+  SetPosition(0,0);
+}
+
+void loop() 
+{
+SetPosition(0,0);
+delay(2000);
+SetPosition(0,-500);
+delay(2000);
+SetPosition(0,600);
+delay(2000);
+SetPosition(0,700);
+delay(2000);
+SetPosition(0,800);
+delay(2000);
+SetPosition(0,1023);
+delay(2000);
+
+}
+
